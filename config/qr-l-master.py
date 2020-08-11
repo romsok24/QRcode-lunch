4f39df5ce4c420046a58b05044f5856de207818c
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-08-11 08:38:20 +0200

    Update qr-l-master.py

--- config/qr-l-master.py
+++ config/qr-l-master.py
@@ -2,12 +2,12 @@
 
 wersja_qrLM = 'Beta 20.0702'
 ile_zetonow_dziennie_max = '2'
-dostawcy=['wyszdr','kntuss']
-pietra=['3b','4a','5a','5b','6b']
+dostawcy=('wyszdr','kntuss')
+pietra=('3b','4a','5a','5b','6b')
 id_zetonu=randomString(8).replace('q','k')
 data_zetonu=datetime.datetime.now().strftime("%Yq%mq%d")
-wiersz_godz=['w1','w2','w3','w4','w5']  # odpowiadają kolejno godzinom 0900-0910, 0910-0920, 0920-0930,0930-0940,0940-0950
-tydzien= ['','','','','']
+wiersz_godz=('w1','w2','w3','w4','w5')  # odpowiadają kolejno godzinom 0900-0910, 0910-0920, 0920-0930,0930-0940,0940-0950
+tydzien= ('','','','','')
 tydzien_pon= {"3b":"w3","4a":"w4","5a":"w5","5b":"w2","6b":"w1"}
 tydzien_wto= {"3b":"w2","4a":"w3","5a":"w4","5b":"w1","6b":"w5"}
 tydzien_sro= {"3b":"w5","4a":"w2","5a":"w1","5b":"w4","6b":"w3"}
